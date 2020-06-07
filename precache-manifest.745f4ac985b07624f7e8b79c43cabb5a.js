cc7caa6093ace345adf7b72442a36256c7788e98
felipe54453 <felipedmlacombe@gmail.com>
2020-06-07 14:36:41 -0300

    Updates

---
+++ precache-manifest.745f4ac985b07624f7e8b79c43cabb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79024f1b40c39ad2c24fd4e7685f9fcd",
+    "url": "/tractian/index.html"
+  },
+  {
+    "revision": "637aba8d47e1629a71d0",
+    "url": "/tractian/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "b946e09a11446b66b438",
+    "url": "/tractian/static/css/main.07ad3eb7.chunk.css"
+  },
+  {
+    "revision": "637aba8d47e1629a71d0",
+    "url": "/tractian/static/js/2.c14c28e4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tractian/static/js/2.c14c28e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b946e09a11446b66b438",
+    "url": "/tractian/static/js/main.0e013beb.chunk.js"
+  },
+  {
+    "revision": "94a6905e5c617dfa3245",
+    "url": "/tractian/static/js/runtime-main.7e7886d4.js"
+  }
+]);
